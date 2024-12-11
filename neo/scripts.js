a4d39390cbc1d657b916f55246b274a5bfe68762
youtubescholar <101683259+youtubescholar@users.noreply.github.com>
2024-12-11 12:13:04 -0500

    Update scripts.js

--- neo/scripts.js
+++ neo/scripts.js
@@ -1,3 +1,4 @@
+<script nonce="abc123">
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('/service-worker.js').then(function(registration) {
         console.log('Service Worker registered with scope:', registration.scope);
@@ -7,13 +8,11 @@ if ('serviceWorker' in navigator) {
 }
 
 function acceptDisclaimer() {
-    // Set a cookie/sessionStorage item
     sessionStorage.setItem('disclaimerAccepted', 'true');
-    // Hide the disclaimer modal
     document.getElementById("disclaimerModal").style.display = "none";
 }
 
 function declineDisclaimer() {
-    // Handle the decline action, such as redirecting to a different page
     window.location.href = '/taichiman/declined/';
 }
+</script>
