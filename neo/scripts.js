8082ebce874a2dd9f3b07705a811755ff803ec33
youtubescholar <101683259+youtubescholar@users.noreply.github.com>
2024-12-11 12:38:27 -0500

    Delete neo/scripts.js

--- neo/scripts.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('acceptButton').addEventListener('click', function () {
-    document.cookie = "disclaimerAccepted=true; path=/; max-age=86400"; // Set cookie
-    document.getElementById("disclaimerModal").style.display = "none"; // Hide modal
-});
-
-document.getElementById('declineButton').addEventListener('click', function () {
-    window.location.href = '/taichiman/declined/';
-});
